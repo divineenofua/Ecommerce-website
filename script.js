ea6d2f23656d141c0361d9c8d50091f53aac5d21
Enofua Divine <divineenofua@gmail.com>
2022-09-22 10:44:52 -1200

    latest commit

--- script.js
+++ script.js
@@ -4,24 +4,24 @@ var loader = document.getElementById("preloader");
 window.addEventListener("load", () => {
 	loader.style.display= "none";
 });
-const modal = doument.getElementById('modal');
-const modalCloseBtn = doument.getElementById('times');
+const modalShow = document.getElementById("modal");
+const modalCloseBtn = document.getElementById('times');
 const openModal =document.getElementById("open-modal");
 
 // MODAL EVENT LISTENERS
-openModal.addEventListener("click", () => {
-	modal.classList.add('show-modal');
-})
-// modalCloseBtn.addEventListener("click", () => {
-// 	modal.classList.remove('show-modal');
+// openModal.addEventListener("click", () => {
+// 	modalShow.classList.add('show-modal');
 // })
-modalCloseBtn.addEventListener('click', () => {
-	modal.style.display = "none";
- });
+modalCloseBtn.addEventListener("click", () => {
+	modalShow.classList.remove('show-modal');
+})
+// modalCloseBtn.addEventListener('click', () => {
+// 	modal.style.display = "none";
+//  });
 
 // notification popup
 const notificationPopUp = document.getElementById("notification");
-const closeNotification = document.querySelector(".uil-times");
+const closeNotification = document.getElementById("notifi-close");
 // notification event listener
 closeNotification.addEventListener("click", () => {
 	notificationPopUp.classList.add('closed');
